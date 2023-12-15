a4ea7b0b2b7b96f46f4b5537021388ef10925569
hirostory <hiroakisotooka@gmail.com>
2023-12-15 15:57:35 -0500

    finally got some direction

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -97,166 +97,198 @@ const NavBar = () => {
 
     const navStyle = {
         background: scrollBackground ? 'white' : 'white',
-        height: '3.5rem',
-        width: 'calc(100% - 2rem)',
+        height: '7rem',
+        width: 'calc(100%)',
         position: 'fixed',
         top: '0',
-        left: '4rem',
         zIndex: '10',
         display: 'flex',
         alignItems: 'center',
         justifyContent: 'space-between',
-        padding: '2rem',
+        // padding: '2rem',
         fontWeight: '500',
-        margin: '1rem 0',
         transition: 'ease-in-out 200ms',
+        // outline: '1px solid black'
       };
-    return (
-        <div>
-                <ul className='mainnav'>
-                    <li>
-                    <Link to={'home'} duration={200} smooth={true} style={{ cursor: "pointer",transition: 'all 0.2s ease-in-out' }}>
-                        <h3>Home</h3>
-                    </Link>
-                    </li>
-                    <li>
-                    <Link to={'about'} duration={200} smooth={true} style={{ cursor: "pointer",transition: 'all 0.2s ease-in-out' }}>
-                        <h3>About</h3>
-                    </Link>
-                    </li>
-                    <li>
-                    <Link to={'projects'} duration={200} smooth={true} style={{ cursor: "pointer",transition: 'all 0.2s ease-in-out' }}>
-                        <h3>Projects</h3>
-                    </Link>
-                    </li>
-                    <li>
-                    <Link to={'resume'} duration={200} smooth={true} style={{ cursor: "pointer",transition: 'all 0.2s ease-in-out' }}>
-                        <h3>Resume</h3>
-                    </Link>
-                    </li>
-                    <li>
-                    <Link to={'contact'} duration={200} smooth={true} style={{ cursor: "pointer",transition: 'all 0.2s ease-in-out' }}>
-                        <h3>Contact</h3>
-                    </Link>
-                    </li>
-                </ul>
-        </div> 
-    )
     // return (
-    //     <nav style={navStyle}>
-    //     {isDesktop && (
-    //       <>
-    //         <Link to='home' duration={0} smooth={true} style={{ cursor: "pointer" }}>
-    //             <div>
-    //                 <h1>hello</h1>
-    //             </div>
-    //         </Link>
-    //         <Link to='about' duration={0} smooth={true} style={{ cursor: "pointer" }}>
-    //             <div>
-    //                 <h1>hello</h1>
-    //             </div>
-    //         </Link>
-    //       </>
-    //     )}
-  
-    //     {!isDesktop && (
-    //       <>
-    //       <Link to='home' duration={0} smooth={true} style={{ cursor: "pointer" }}>
-    //         <div className='flex items-center justify-center'>
-    //           <div className='flex flex-col justify-center'>
-    //             <h1 className='font-black text-primary-green'>Neurosynth</h1>
-    //             <h1 className='font-normal'>Dynamics</h1>
-    //           </div>
-    //         </div>
-    //       </Link>
-    //       <div className='flex items-center gap-4 text-sm'>
-    //         <Link
-    //           to='about'
-    //           duration={0}
-    //           smooth={true}
-    //           className='cursor-pointer text-base hover:text-primary-green transition duration-200'
-    //         >
-    //           Learn More
-    //         </Link>
-    //         <Link to='contact' duration={0} smooth={true} className=''>
-    //           <button className='bg-primary-green text-white hover:bg-green-700 px-3.5 py-2.5 transition duration-200'>
-    //             Contact Us
-    //           </button>
-    //         </Link>
-    //       </div>
-    //     </>
-    //     )}
-    //     { !isDesktop && (
-    //         <div className='flex items-center justify-between'>
-    //         <Link
-    //           to='home'
-    //           smooth={true}
-    //           duration={0}
-    //           style={{ cursor: "pointer" }}
-    //           onClick={() => setToggled(false)}
-    //         >
-    //           <div className='flex items-center justify-center'>
-    //             <div className='flex flex-col justify-center'>
-    //               <h1 className='font-black text-primary-green'>Neurosynth</h1>
-    //               <h1 className='font-normal'>Dynamics</h1>
-    //             </div>
-    //           </div>
-    //         </Link>
-    //         <div
-    //           onClick={() => setToggled((prevToggled) => !prevToggled)}
-    //           className='space-y-1.5 cursor-pointer'
-    //         >
-    //           <motion.span
-    //             animate={{ rotateZ: toggled ? 45 : 0, y: toggled ? 8 : 0 }}
-    //             className='block h-0.5 w-8 bg-black'
-    //           ></motion.span>
-    //           <motion.span
-    //             animate={{ scale: toggled ? 0 : 1 }}
-    //             className='block h-0.5 w-8 bg-black'
-    //           ></motion.span>
-    //           <motion.span
-    //             animate={{
-    //               rotateZ: toggled ? -45 : 0,
-    //               y: toggled ? -8 : 0,
-    //             }}
-    //             className='block h-0.5 w-8 bg-black'
-    //           ></motion.span>
-    //         </div>
-    //       </div>
-    //     )}
-    //     <AnimatePresence>
-    //     {toggled && !isDesktop && (
-    //     <motion.div
-    //         variants={navMotion}
-    //         initial='hidden'
-    //         animate='visible'
-    //         exit='exit'
-    //         className='fixed top-[80px] left-4 w-[calc(100%-2rem)] h-screen flex items-center justify-center'
-    //     >
-    //         <div className='flex flex-col items-center justify-center gap-12 h-full'>
-    //         <motion.div variants={navItemMotion}>
-    //             <Link
-    //             to='about'
-    //             smooth={true}
-    //             style={{ cursor: "pointer" }}
-    //             onClick={() => setToggled(false)}
-    //             >
-    //             Learn More
-    //             </Link>
-    //         </motion.div>
-    //         <motion.div variants={navItemMotion}>
-    //             <Link to='contact' smooth={true} onClick={() => setToggled(false)}>
-    //             <button className='bg-primary-green text-white hover:bg-green-700 px-3.5 py-2.5 transition duration-200'>
-    //                 Join Trial
-    //             </button>
-    //             </Link>
-    //         </motion.div>
-    //         </div>
-    //     </motion.div>
-    //     )}
-    // </AnimatePresence>
-    //   </nav>
+    //     <div>
+    //             <ul className='mainnav'>
+    //                 <li>
+    //                 <Link to={'home'} duration={200} smooth={true} style={{ cursor: "pointer",transition: 'all 0.2s ease-in-out' }}>
+    //                     <h3>Home</h3>
+    //                 </Link>
+    //                 </li>
+    //                 <li>
+    //                 <Link to={'about'} duration={200} smooth={true} style={{ cursor: "pointer",transition: 'all 0.2s ease-in-out' }}>
+    //                     <h3>About</h3>
+    //                 </Link>
+    //                 </li>
+    //                 <li>
+    //                 <Link to={'projects'} duration={200} smooth={true} style={{ cursor: "pointer",transition: 'all 0.2s ease-in-out' }}>
+    //                     <h3>Projects</h3>
+    //                 </Link>
+    //                 </li>
+    //                 <li>
+    //                 <Link to={'resume'} duration={200} smooth={true} style={{ cursor: "pointer",transition: 'all 0.2s ease-in-out' }}>
+    //                     <h3>Resume</h3>
+    //                 </Link>
+    //                 </li>
+    //                 <li>
+    //                 <Link to={'contact'} duration={200} smooth={true} style={{ cursor: "pointer",transition: 'all 0.2s ease-in-out' }}>
+    //                     <h3>Contact</h3>
+    //                 </Link>
+    //                 </li>
+    //             </ul>
+    //     </div> 
     // )
+    return (
+        <nav style={navStyle}>
+        {/* {isDesktop && (
+            <>
+                <Link to='home' duration={0} smooth={true} style={{ textDecoration: 'none', color: 'inherit' }}>
+                <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+                    <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center'}}>
+                    <h1 style={{ fontWeight: 'bold', color: 'green' }}>Hiroaki</h1>
+                    <h1 style={{ fontWeight: 'normal' }}>Sotooka</h1>
+                    </div>
+                </div>
+                </Link>
+                <div style={{ display: 'flex', alignItems: 'center', gap: '16px', fontSize: '14px' }}>
+                <Link
+                    to='about'
+                    duration={0}
+                    smooth={true}
+                    style={{ textDecoration: 'none', color: 'inherit' }}
+                >
+                    Learn More Love
+                </Link>
+                <Link
+                    to='projects'
+                    duration={0}
+                    smooth={true}
+                    style={{ textDecoration: 'none', color: 'inherit' }}
+                >
+                    <h3>projects</h3>
+                </Link>
+                <Link to='contact' duration={0} smooth={true} style={{ textDecoration: 'none' }}>
+                    <button
+                    style={{
+                        backgroundColor: 'green',
+                        color: 'white',
+                        padding: '8px 16px',
+                        borderRadius: '4px',
+                        boxShadow: '2px 2px 4px rgba(0, 0, 0, 0.2)',
+                        transition: 'background-color 0.3s, box-shadow 0.3s, color 0.3s',
+                        border: 'none',
+                        cursor: 'pointer',
+                    }}
+                    >
+                    Contact Us
+                    </button>
+                </Link>
+                </div>
+            </>
+            )} */}
+
+            {/* navbar for tablet and below */}
+            {/* {!isDesktop && ( */}
+            <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'column', height: '100%', width: '100%', flexDirection: 'column' }}>
+                <Link
+                to='home'
+                smooth={true}
+                duration={0}
+                style={{ textDecoration: 'none', color: 'inherit' }}
+                onClick={() => setToggled(false)}
+                >
+                <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column' }}>
+                    <div className="block"> <img  src="https://see.fontimg.com/api/renderfont4/6jRq/eyJyIjoiZnMiLCJoIjoxMzAsInciOjIwMDAsImZzIjo2NSwiZmdjIjoiI0Y2Q0E4RSIsImJnYyI6IiNGRkZGRkYiLCJ0IjoxfQ/OQ/nymphette.png"/> </div>
+                    <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center'}}>
+                    <img style={{ fontWeight: 'bold', height: '3rem', marginTop:'1rem', }} src="https://see.fontimg.com/api/renderfont4/d9z4R/eyJyIjoiZnMiLCJoIjoxNTYsInciOjIwMDAsImZzIjo3OCwiZmdjIjoiIzM2NEI1MCIsImJnYyI6IiNGRkZGRkYiLCJ0IjoxfQ/SGlyb2FraSBTb3Rvb2th/hello-good-old-style-regular.png"  ></img>
+                    {/* <h1 style={{ fontWeight: 'normal' }}>Sotooka</h1> */}
+                    </div>
+                    <div className="thin-line"></div>
+                    <div className="block-2"> <img  src="https://see.fontimg.com/api/renderfont4/6jRq/eyJyIjoiZnMiLCJoIjoxMzAsInciOjIwMDAsImZzIjo2NSwiZmdjIjoiI0Y2Q0E4RSIsImJnYyI6IiNGRkZGRkYiLCJ0IjoxfQ/OQ/nymphette.png"/> </div>
+                </div>
+                </Link>
+                <div
+                onClick={() => setToggled((prevToggled) => !prevToggled)}
+                style={{ display: 'flex', flexDirection: 'column', cursor: 'pointer', gap: '5px' }}
+                >
+                    <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', cursor: 'pointer', margin: '.5rem' }} >
+                    <img src="https://see.fontimg.com/api/renderfont4/L36OZ/eyJyIjoiZnMiLCJoIjoxNTYsInciOjIwMDAsImZzIjo3OCwiZmdjIjoiIzM2NEI1MCIsImJnYyI6IiNGRkZGRkYiLCJ0IjoxfQ/eQ/font-frame-pro.png" style={{  width: '7rem', transform: toggled ? 'scale(0)' : 'scale(1)', transition: 'transform 0.3s' }} ></img>
+                    <img src="https://see.fontimg.com/api/renderfont4/d9z4R/eyJyIjoiZnMiLCJoIjoxNTYsInciOjIwMDAsImZzIjo3OCwiZmdjIjoiIzM2NEI1MCIsImJnYyI6IiNGRkZGRkYiLCJ0IjoxfQ/TWVudQ/hello-good-old-style-regular.png" style={{  position: 'absolute', width: '3rem', transform: toggled ? 'scale(0)' : 'scale(1)', transition: 'transform 0.3s' }} ></img>
+                    </div>
+                {/* <img src="https://see.fontimg.com/api/renderfont4/r7rL/eyJyIjoiZnMiLCJoIjoxODYsInciOjIwMDAsImZzIjo5MywiZmdjIjoiIzAzMDQwNCIsImJnYyI6IiNGRkZGRkYiLCJ0IjoxfQ/Yw/rta-ermine.png" style={{  width: '30px', transform: toggled ? 'scale(0)' : 'scale(1)', transition: 'transform 0.3s' }} ></img> */}
+                {/* <span style={{ display: 'block', height: '2px', width: '24px', backgroundColor: 'black', transform: toggled ? 'scale(0)' : 'scale(1)', transition: 'transform 0.3s' }}></span>
+                <span style={{ display: 'block', height: '2px', width: '24px', backgroundColor: 'black', transform: toggled ? 'scale(0)' : 'scale(1)', transition: 'transform 0.3s' }}></span>
+                <span style={{ display: 'block', height: '2px', width: '24px', backgroundColor: 'black', transform: toggled ? 'scale(0)' : 'scale(1)', transition: 'transform 0.3s' }}></span> */}
+                </div>
+            </div>
+            {/* )} */}
+
+            {/* hamburger menu */}
+            <AnimatePresence>
+            {toggled && (
+                <motion.div
+                variants={navMotion}
+                initial='hidden'
+                animate='visible'
+                exit='exit'
+                style={{
+                    position: 'fixed',
+                    top: '120px',
+                    left: '12px',
+                    width: 'calc(100% - 2rem)',
+                    height: '100vh',
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    zIndex: '10',
+                    boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.1)',
+                    backgroundColor: 'white',
+                    outline: '1px solid black',
+                }}
+                >
+                <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', marginTop: '-8rem', gap: "3rem", height: '100%' }}>
+                    <Link
+                    to='about'
+                    smooth={true}
+                    style={{ textDecoration: 'none', color: 'inherit', fontSize: '24px' }}
+                    onClick={() => setToggled(false)}
+                    >
+                    <h1>About</h1>
+                    </Link>
+                    <Link
+                    to="projects"
+                    smooth={true}
+                    style={{ textDecoration: 'none', color: 'inherit', fontSize: '24px' }}
+                    onClick={() => setToggled(false)}
+                    >
+                    <h1>Projects</h1>
+                    </Link>
+                    <Link
+                    to="resume"
+                    smooth={true}
+                    style={{ textDecoration: 'none', color: 'inherit', fontSize: '24px' }}
+                    onClick={() => setToggled(false)}
+                    >
+                    <h1>Resume</h1>
+                    </Link>
+                    <Link
+                    to="contact"
+                    smooth={true}
+                    style={{ textDecoration: 'none', color: 'inherit', fontSize: '24px' }}
+                    onClick={() => setToggled(false)}
+                    >
+                    <h1>Contact</h1>
+                    </Link>
+                </div>
+                </motion.div>
+            )}
+            </AnimatePresence>
+
+      </nav>
+    )
 }
 
 export default NavBar
