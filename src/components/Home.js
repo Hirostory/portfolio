67fdcc6fbb914d8ca86ba688ec1cbe44ed97f9ad
hirostory <hiroakisotooka@gmail.com>
2023-12-15 23:38:36 -0500

    MVP for portfoli

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,21 +2,31 @@ import { Link } from "react-scroll"
 
 const Home = () => {
     return (
-        <div id='home' style={{ minHeight: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+        <div id='home' style={{ minHeight: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column' }}>
         <div className/>
-          <h1 
-            data-aos='fade-up'
-            data-aos-duration='500'
-            data-aos-delay='200'
-            data-aos-once='true' >
-            Love
-          </h1>
-          <h1>
-            Dynamics
-          </h1>
-          <p>
-            Engineering Evolution, Beyond Imagination
+          
+          <img 
+          data-aos='fade-right'
+          data-aos-duration='500'
+          data-aos-delay='200'
+          data-aos-once='true' 
+          src="https://see.fontimg.com/api/renderfont4/d9z4R/eyJyIjoiZnMiLCJoIjozMjYsInciOjIwMDAsImZzIjoxNjMsImZnYyI6IiMyOTNCMzciLCJiZ2MiOiIjRkZGRkZGIiwidCI6MX0/SElSTw/hello-good-old-style-regular.png" className="main-main-logo" />
+           <p 
+           data-aos='fade-left'
+           data-aos-duration='500'
+           data-aos-delay='200'
+           data-aos-once='true' 
+           className="tagline">
+          Crafting Code, Creating Tomorrow
           </p>
+          <img 
+          data-aos='fade-in'
+          data-aos-duration='500'
+          data-aos-delay='200'
+          data-aos-once='true' 
+          style={{opacity: '.3'}}
+          src="https://see.fontimg.com/api/renderfont4/Yqa/eyJyIjoiZnMiLCJoIjo0MDAsInciOjIwMDAsImZzIjoyMDAsImZnYyI6IiMyOTNCMzciLCJiZ2MiOiIjRkZGRkZGIiwidCI6MX0/Tw/4yeogarden.png" className="tree-main"/>
+         
         </div>
     )
 }
